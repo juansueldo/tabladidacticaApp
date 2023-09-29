ca4391508efb3f52fa3fc5712f04350e94df06ae
unknown <juan.sueldo1989@gmail.com>
2023-09-29 01:39:22 -0300

    prueba

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,30 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'tabladidacticaApp',
+  appId: 'io.ionic.tabladidacticaapp',
+  appName: 'TablaDidacticaApp',
   webDir: 'www',
   server: {
     androidScheme: 'https'
+  },plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 3000,
+      backgroundColor: "#fad06a",
+      androidSplashResourceName: "splash",
+      //androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#fad06a",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: true,
+    },
   }
+
 };
 
 export default config;
