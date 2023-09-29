6979873700579d0d6bb351961c7a448191f56251
unknown <juan.sueldo1989@gmail.com>
2023-09-29 02:09:58 -0300

    arreglos

--- src/app/pages/splash-screen/splash-screen.page.ts
+++ src/app/pages/splash-screen/splash-screen.page.ts
@@ -9,11 +9,7 @@ import { Router } from '@angular/router';
 export class SplashScreenPage implements OnInit {
 
   constructor(private route: Router) {
-<<<<<<< HEAD
-    setTimeout(() => {
-=======
    setTimeout(() => {
->>>>>>> d64588022edcb74922cf32a34184e79f2923d45e
       this.route.navigateByUrl('/login');
     },2500);
    }
